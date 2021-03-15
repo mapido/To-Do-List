fb9aec1d662d9fc8b4e72ba4cfe5d8daad0837f0
mapido <78904755+mapido@users.noreply.github.com>
2021-03-15 20:11:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,38 @@
 const input = document.querySelector("input");
 const button = document.querySelector("button");
-const list = document.querySelector("div");
+const list = document.querySelector("ul");
 
-// Adds a list element when the "+" button is pressed.
-button.addEventListener("click", function () {
-    let element = document.createElement("li");
+// Main function
+function tasks() {
+
+    // Adds a list element
+    const element = document.createElement("li");
     list.appendChild(element);
     element.innerText = input.value;
     input.value = "";
 
-    // Marks the element as done when clicked.
+    // Marks the element as "done" when clicked.
     element.addEventListener("click", function () {
         element.style.textDecoration = "line-through";
     });
 
     // Removes the element with a double click.
-    element.addEventListener("dblclick", function(){
+    element.addEventListener("dblclick", function () {
         list.removeChild(element);
     });
+}
+
+
+// Adds a list element when the "+" button is pressed.
+button.addEventListener("click", function () {
+    tasks()
 });
 
 
-// Creates a list element when the "Enter" key is pressed.
-document.addEventListener("keydown", function (event) {
-    const keyName = event.key;
-    let enterElement = document.createElement("li");
-    if (keyName === "Enter") {
-        list.appendChild(enterElement);
-        enterElement.innerText = input.value;
-        input.value = "";
-    };
-
-    // 
-    enterElement.addEventListener("click", function () {
-        enterElement.style.textDecoration = "line-through";
-    });
-
-    // 
-    enterElement.addEventListener("dblclick", function(){
-        list.removeChild(enterElement);
-    });
+// Adds a list element when the "Enter" key is pressed.
+document.addEventListener("keydown", function () {
+    const keyEvent = event.key;
+    if (keyEvent === "Enter") {
+        tasks();
+    }
 });
